32f4ad1a5e12ce7e00c6655f81ea00bfc951def7
Life is a ninanino <tvxqinak@naver.com>
2023-04-28 11:57:49 +0900

    nino 요구사항 3 완료

--- src/test/java/next/reflection/Junit4TestRunner.java
+++ src/test/java/next/reflection/Junit4TestRunner.java
@@ -2,10 +2,28 @@ package next.reflection;
 
 import org.junit.Test;
 
+import java.lang.reflect.Array;
+import java.lang.reflect.InvocationTargetException;
+import java.lang.reflect.Method;
+import java.util.Arrays;
+
 public class Junit4TestRunner {
     @Test
     public void run() throws Exception {
         Class<Junit4Test> clazz = Junit4Test.class;
-        // TODO Junit4Test에서 @MyTest 애노테이션이 있는 메소드 실행
+
+        Junit4Test junit4Test = clazz.getDeclaredConstructor().newInstance();
+        Method[] methods = clazz.getDeclaredMethods();
+        Arrays.stream(methods)
+                .filter(m -> m.isAnnotationPresent(MyTest.class))
+                .forEach(m -> {
+                    try {
+                        m.invoke(junit4Test);
+                    } catch (IllegalAccessException e) {
+                        throw new RuntimeException(e);
+                    } catch (InvocationTargetException e) {
+                        throw new RuntimeException(e);
+                    }
+                });
     }
 }
