5f48c61802b0464c0bf23efd8333b10f947cb2b6
saehim <saeilee.vxv@gmail.com>
2023-04-28 11:46:54 +0900

    iirin 요구사항2 완료

--- src/test/java/next/reflection/Junit3TestRunner.java
+++ src/test/java/next/reflection/Junit3TestRunner.java
@@ -2,10 +2,25 @@ package next.reflection;
 
 import org.junit.Test;
 
+import java.lang.reflect.InvocationTargetException;
+import java.lang.reflect.Method;
+import java.util.Arrays;
+
 public class Junit3TestRunner {
     @Test
     public void run() throws Exception {
         Class<Junit3Test> clazz = Junit3Test.class;
-        // TODO Junit3Test에서 test로 시작하는 메소드 실행
+        Method[] declaredMethods = clazz.getDeclaredMethods();
+
+        Junit3Test junit3Test = clazz.getDeclaredConstructor().newInstance();
+
+        Arrays.stream(declaredMethods).filter(m -> m.getName().startsWith("test"))
+                .forEach(m -> {
+                    try {
+                        m.invoke(junit3Test);
+                    } catch (IllegalAccessException | InvocationTargetException e) {
+                        throw new RuntimeException(e);
+                    }
+                });
     }
 }
